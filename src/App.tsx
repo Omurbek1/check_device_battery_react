15d07a204c6400a8ec787f8ae1b6650dd9e0adae
Omur <omurbekmamytbekov545@gmail.com>
2023-10-18 13:07:16 +0600

    check battery devices

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { useBattery } from 'react-use'
 
 
 function App() {
+  // Вызов хука useBattery 
   const battery=useBattery()
+
+    // Деструктуризация свойств батареи 
   const {isSupported,level, charging, dischargingTime, chargingTime}=battery;
+
+   //Проверяем, поддерживает ли браузер API батареи 
   if(!isSupported){
     return(
       <div>
@@ -14,12 +19,16 @@ function App() {
   }
   return (
     <div>
+       {/* Отобразить процент заряда батареи */} 
       <strong>Уровень заряда -</strong>
       <span>{(level*100).toFixed(0)} %</span><br/>
+      {/* Отображение того, заряжается ли устройство */ }
       <strong>Зарядка</strong>:  {" "} 
       <span>{charging ? "Да" : "Нет "}</span> <br /> 
+       {/* Время зарядки устройства отображения */} 
       <strong>Время зарядки</strong>:   
       <span>{chargingTime ? chargingTime : "Finished"}</span> <br />
+       {/* Время разрядки устройства отображения */} 
       <strong>Время разрядки</strong>:&nbsp;&nbsp;{" "}
       <span>{dischargingTime}</span>
     </div>
